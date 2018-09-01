f939319d1778396f2440a040505465997f6eaa94
0rC <13_0rC@mail.ru>
2018-09-01 19:26:24 +0300

    new repository

---
+++ less_7/less7.js
@@ -0,0 +1,8 @@
+var but= document.querySelector('button');
+		but.onclick= function()
+		{
+			if(but.style.fontSize='blue')
+				{ but.style.backgroundColor='blue';}
+			else{
+				but.style.backgroundColor='#00FF00'
+			}
